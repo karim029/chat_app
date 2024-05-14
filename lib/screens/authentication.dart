f8dd25573e96c00748cae4bb533ceb5baaefceff
karim <karim.tarek96@yahoo.com>
2024-05-14 18:21:23 +0300

    first commit

---
+++ lib/screens/authentication.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AuthenticationScreen extends StatefulWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  State<AuthenticationScreen> createState() {
+    return _AuthenticationScreenState();
+  }
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
