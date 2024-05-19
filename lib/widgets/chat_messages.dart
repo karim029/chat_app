5eca4da5ca2bd3f581e3f41fa832dbe0bc3905ac
karim <karim.tarek96@yahoo.com>
2024-05-19 17:28:29 +0300

    added a streamBuilder in chat_messages.dart

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,13 +1,41 @@
-
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatMessages extends StatelessWidget{
+class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
-
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('no messages found.'),);
-   }
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            return Text(
+              loadedMessages[index].data()['text'],
+            );
+          },
+        );
+      },
+    );
+  }
 }
